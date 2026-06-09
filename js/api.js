f79a07ebbf4bd77de23c115b4e416f6fe1e2391c
LamiaPN <lamia@printempsnumerique.ca>
2026-06-09 15:29:48 -0400

    Add files via upload

---
+++ js/api.js
@@ -0,0 +1,5 @@
+/**
+ * URL de l'API Google Apps Script
+ */
+const API_URL =
+"https://script.google.com/macros/s/AKfycbwGr3X6O2QVq4Uy1aGm6gZj1DnTbJdeosZVC0mYEmVEUUsOSGQT20GCvHyIniMks9VhZA/exec";
---
+++ js/config.js
@@ -0,0 +1,13 @@
+const APP_CONFIG = {
+
+    eventName: "MTL connecte 2026",
+
+    ticketPrefix: "T",
+
+    bannerImage: "assets/banner.png",
+
+    allowDuplicateEmails: false,
+
+    localStorageKey: "mtl_tombola_participants"
+
+};
